8aee759d8d28086496ffd8265d2c3801b5d7153e
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2025-11-25 21:45:43 -0300

    Pipes funcionality created, game over and hit collision created. Starting with animation and sprite scaling

--- 1 - Flappy bird/game.py
+++ 1 - Flappy bird/game.py
@@ -1,3 +1,4 @@
+import sys, os
 import pygame
 from pipe import Pipe
 
@@ -28,44 +29,105 @@ bird_rect = pygame.Rect(
     bird_pos_x, bird_pos_y, bird_size, bird_size
     )
 
-pipe = Pipe(WIDTH)
+pipes = []
+score = 0
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+assets_dir = os.path.join(script_dir, 'assets')
+
+bird_assets = [
+    pygame.image.load(os.path.join(assets_dir, 'bird1.png')).convert_alpha(),
+    pygame.image.load(os.path.join(assets_dir, 'bird2.png')).convert_alpha(),
+    pygame.image.load(os.path.join(assets_dir, 'bird3.png')).convert_alpha()
+]
+bird_img_index=0
+bird_img = bird_assets[bird_img_index]  
+
+
+GAME_OVER = False
+
+try:
+    font = pygame.font.SysFont('Arial', 40, bold=True)
+except:
+    font = pygame.font.Font(None, 40)
 
 running = True
 velocity_y = 0
+
+def generate_new_pipes():
+    new_pipe=Pipe(WIDTH+50)
+    pipes.append(new_pipe)
+
+def show_score():
+    score_text = font.render(f"Score: {score}", True, WHITE)
+    SCREEN.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, 20))
+
+generate_new_pipes()
+
+last_pipe_time = pygame.time.get_ticks()
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
         
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and not GAME_OVER:
             if event.key == pygame.K_SPACE:
                 velocity_y = VELOCITY
 
+    if not GAME_OVER:
+
+        velocity_y += GRAVITY
+
+        if velocity_y >10:
+            velocity_y = 10
+        
+        bird_rect.y += velocity_y
+
+        if bird_rect.bottom >= HEIGHT:
+            bird_rect.bottom = HEIGHT
+            GAME_OVER = True
+
+        elif bird_rect.top <= 0:
+            bird_rect.top = 0
+            GAME_OVER = True
+
+        current_time = pygame.time.get_ticks()
+        
+        if current_time - last_pipe_time > 2500:
+            generate_new_pipes()
+            last_pipe_time = current_time
+
+        
+        SCREEN.fill(BLUE_SKY)
+        pygame.draw.rect(SCREEN, WHITE, bird_rect)
+        for pipe in pipes:
+            pipe.move()
+            pipe.draw(SCREEN)
+            if pipe.check_collision(bird_rect):
+                GAME_OVER = True
+
+            if pipe.pos_x + pipe.width < bird_rect.x and not pipe.passed:
+                score += 1
+                pipe.passed = True
+
+        show_score()
+
+        for pipe in pipes:
+            if pipe.pos_x + pipe.width < 0:
+                pipes.remove(pipe)
+
+        if GAME_OVER:
+            game_over_text = font.render("Game Over", True, WHITE)
+            SCREEN.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 2 - game_over_text.get_height() // 2))
+            
 
 
+        pygame.display.flip()
 
-    velocity_y += GRAVITY
-
-    if velocity_y >10:
-        velocity_y = 10
-    
-    bird_rect.y += velocity_y
-
-    if bird_rect.bottom >= HEIGHT:
-        bird_rect.bottom = HEIGHT
-        velocity_y = 0
-    elif bird_rect.top <= 0:
-        bird_rect.top = 0
-        velocity_y = 0
-    
-    pipe.move()
-
-    SCREEN.fill(BLUE_SKY)
-    pygame.draw.rect(SCREEN, WHITE, bird_rect)
-    pipe.draw(SCREEN)
-    pygame.display.flip()
-
-    CLOCK.tick(FPS)
+        CLOCK.tick(FPS)
 
 pygame.quit()
 quit()
--- 1 - Flappy bird/pipe.py
+++ 1 - Flappy bird/pipe.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os
 
 HEIGH_SPACE = 200
 HEIGHT = 800
@@ -10,22 +11,40 @@ class Pipe:
 
         self.pos_x = x
         self.passed = False
+        self.width = 70
+
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        assets_dir = os.path.join(script_dir, 'assets')
+        pipe_img_path = os.path.join(assets_dir, 'pipe.png')
+        self.pipe_img = pygame.image.load(pipe_img_path).convert_alpha()
+        self.pipe_img = pygame.transform.scale(self.pipe_img, (self.width, self.pipe_img.get_height()))
+        self.pipe_img_rotated = pygame.transform.scale(self.pipe_img, (self.width, self.pipe_img.get_height()))
+
+
 
         center_space_y = random.randint(HEIGH_SPACE, HEIGHT - HEIGH_SPACE)
         self.top_pipe_rect = pygame.Rect(
-            self.pos_x, 0, 70, center_space_y-HEIGH_SPACE 
+            self.pos_x, 0, self.width, center_space_y-HEIGH_SPACE 
         )
 
         self.bottom_pipe_rect = pygame.Rect(
-            self.pos_x, center_space_y + HEIGH_SPACE, 70, HEIGHT - (center_space_y + HEIGH_SPACE)
+            self.pos_x, center_space_y + HEIGH_SPACE, self.width, HEIGHT - (center_space_y + HEIGH_SPACE)
         )
-        print(center_space_y)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (100, 255, 0), self.top_pipe_rect)
-        pygame.draw.rect(screen, (100, 255, 0), self.bottom_pipe_rect)
+        screen.blit(self.pipe_img_rotated, self.top_pipe_rect)
+        screen.blit(self.pipe_img, self.bottom_pipe_rect)
+
 
     def move(self):
-        self.pos_x -= 5
+        self.pos_x -= 4
         self.top_pipe_rect.x = self.pos_x
         self.bottom_pipe_rect.x = self.pos_x
+
+    def check_collision(self, bird_rect):
+        if self.top_pipe_rect.colliderect(bird_rect) or self.bottom_pipe_rect.colliderect(bird_rect):
+            return True
+        else:
+            return False
+
+
